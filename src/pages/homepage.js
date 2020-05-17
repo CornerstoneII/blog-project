3e6782489502f2e89e3421dc4777bbd5ac6b828d
CornerstoneII <seunmyles@gmail.com>
2020-05-17 13:26:10 +0100

    creating-the-app-component

---
+++ src/pages/homepage.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1> Hi there, welcome to my blog!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis
+      facilisis, consectetur urna at, vehicula arcu. Maecenas a dui in nisl
+      aliquet tempor non a ante. Suspendisse potenti. Pellentesque vehicula,
+      nisl eget faucibus pharetra, augue purus rhoncus nunc, eget posuere urna
+      ante eget sapien. Curabitur dictum fermentum odio, eu gravida nibh tempor
+      id. Nullam a dolor non velit egestas convallis. Vestibulum et justo
+      posuere, aliquam risus non, tincidunt risus. Pellentesque neque enim,
+      placerat non luctus non, aliquet eget metus. Praesent in velit odio.
+      Suspendisse potenti. Nam ornare lectus ac dui semper rhoncus. Nulla vitae
+      aliquam quam. Aenean felis leo, fringilla non urna et, suscipit dapibus
+      urna. Aenean pharetra et lorem id placerat. Nullam tempus mollis gravida.
+      Fusce venenatis sapien ac diam suscipit, id condimentum nibh finibus.
+    </p>
+    <p>
+      Nunc condimentum magna libero. Mauris lacinia libero et aliquet mattis.
+      Aenean euismod fermentum dolor, quis consequat ligula finibus venenatis.
+      Mauris porta purus vehicula urna aliquet, a euismod sem sollicitudin.
+      Donec varius diam convallis sodales tristique. Nullam laoreet quis turpis
+      non tempor. Nam quis enim sed lacus rhoncus lobortis ultrices gravida
+      erat. Nullam consequat ornare sem, eu egestas est lobortis quis. Cras
+      ullamcorper nibh sed dignissim pretium. Pellentesque vitae elit at libero
+      tincidunt faucibus. Vestibulum interdum nisi quis consequat dapibus.
+      Suspendisse lobortis convallis ante ac condimentum. Pellentesque rutrum
+      non sapien quis egestas. Donec sit amet ante sollicitudin, commodo ipsum
+      ut, aliquam nulla. Cras hendrerit sodales eleifend. Proin aliquet odio at
+      libero lobortis feugiat eu at lacus.
+    </p>
+    <p>
+      Nam iaculis eleifend consequat. Integer eget diam pharetra, sodales elit
+      vel, accumsan leo. Quisque blandit, leo eget mattis aliquet, sem eros
+      ultricies libero, vitae sagittis ipsum risus rhoncus enim. Donec posuere,
+      ligula vitae luctus bibendum, felis erat sodales justo, vitae finibus nisl
+      ligula quis ante. Suspendisse quis auctor turpis. Vivamus non iaculis
+      lectus, et dignissim urna. Aliquam massa eros, lobortis aliquet augue non,
+      sollicitudin consequat nunc. Phasellus quis finibus enim. Curabitur
+      ullamcorper arcu enim, quis facilisis velit gravida ut. Pellentesque
+      habitant morbi tristique senectus et netus et malesuada fames ac turpis
+      egestas. Sed sit amet odio eros. Etiam eros ex, imperdiet non interdum
+      sed, iaculis vel tortor. Cras rhoncus placerat est in efficitur.
+    </p>
+    <p>
+      Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+      ridiculus mus. Sed quis nulla vitae tortor congue dapibus. Vivamus vitae
+      lorem eget turpis tempus tempor at ut neque. Mauris sagittis sem faucibus
+      porttitor fringilla. Phasellus turpis tortor, venenatis sagittis ex vel,
+      pretium mollis mi. Fusce hendrerit magna a est imperdiet, vel suscipit ex
+      ornare. Etiam luctus consequat scelerisque. Suspendisse tristique molestie
+      nulla vel volutpat. Cras et venenatis dui, at mattis neque.
+    </p>
+    <p>
+      Cras scelerisque dignissim mi, sed eleifend nisl molestie non. Aliquam
+      neque sem, tristique in ligula quis, tempus lobortis tortor. Curabitur eu
+      augue sed magna vehicula cursus eu et odio. Duis fringilla facilisis magna
+      nec venenatis. Curabitur cursus pretium elit sit amet volutpat. Cras id
+      purus et lacus fermentum ultricies in vitae nibh. In sagittis dapibus
+      dictum. Duis non turpis ultricies, consectetur felis eget, aliquet elit.
+    </p>
+  </>
+);
+
+export default HomePage;
