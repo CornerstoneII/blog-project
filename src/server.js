15ba08d0df5994ce24d0d929ddc748efe4f7b821
CornerstoneII <seunmyles@gmail.com>
2020-05-18 00:27:05 +0100

    adding-mongo-to-express

--- src/server.js
+++ src/server.js
@@ -1,26 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-const articlesInfo = {
-  "learn-react": {
-    upvotes: 0,
-    comments: [],
-  },
-  "learn-node": {
-    upvotes: 0,
-    comments: [],
-  },
-  "my-thoughts-on-resumes": {
-    upvotes: 0,
-    comments: [],
-  },
-};
+import { MongoClient } from "mongodb";
 
 const app = express();
 
 const PORT = 7070;
 
 app.use(bodyParser.json());
+
+// Fetch article Name End Point
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db("blog-project");
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
 
 // Upvoting End Points
 
