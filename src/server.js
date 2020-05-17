89f7775bc00237d131cc0ccea3a5c2f28e2e7f71
CornerstoneII <seunmyles@gmail.com>
2020-05-17 20:01:17 +0100

    updating-articles

--- src/server.js
+++ src/server.js
@@ -1,9 +1,37 @@
 import express from "express";
+import bodyParser from "body-parser";
+
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+  },
+  "learn-node": {
+    upvotes: 0,
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+  },
+};
 
 const app = express();
 
 const PORT = 7070;
 
-app.get("/hello", (req, res) => res.send("Hello"));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].upvotes += 1;
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
+});
+
+// app.get("/hello", (req, res) => res.send("Hello"));
+// app.get("/hello/:name", (req, res) => res.send(`Hello, ${req.params.name}`));
+// app.post("/hello", (req, res) => res.send(`Hello, ${req.body.name}`));
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
