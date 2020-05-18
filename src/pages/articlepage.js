fcfa4fbb36c4652a24cea7618d7b5d040768d365
CornerstoneII <seunmyles@gmail.com>
2020-05-18 21:21:45 +0100

    displaying-comments

--- src/pages/articlepage.js
+++ src/pages/articlepage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import NotFoundPage from "./notfoundpage";
 import ArticleList from "../components/articleslist";
+import CommentsList from "../components/commentslist";
 import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
@@ -13,7 +14,7 @@ const ArticlePage = ({ match }) => {
     const fetchData = async () => {
       const result = await fetch(`/api/articles/${name}`);
       const body = await result.json();
-
+      console.log(body);
       setArticleInfo(body);
     };
     fetchData();
@@ -31,6 +32,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Related Articles:</h3>
       <ArticleList articles={otherArticles} />
     </>
